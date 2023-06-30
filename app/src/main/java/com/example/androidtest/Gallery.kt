1e1a4fe117ef5e50e50fb7520d8ff99030659d61
seyeoncho33 <135544903+seyeoncho33@users.noreply.github.com>
2023-06-30 16:12:05 +0900

    test

--- app/src/main/java/com/example/androidtest/Gallery.kt
+++ app/src/main/java/com/example/androidtest/Gallery.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidtest.databinding.FragmentGalleryBinding
 
-class Gallery :Fragment(){
+class Gallery :Fragment(){ //dkdkdkdkd
     private lateinit var binding: FragmentGalleryBinding
     override fun onCreateView(
         inflater:LayoutInflater,
