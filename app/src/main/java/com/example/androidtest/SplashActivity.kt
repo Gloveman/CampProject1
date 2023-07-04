a8eabb463a3273a428a8832bb56f4ef2fbc4a7fb
gloveman <gloveman@postech.ac.kr>
2023-07-05 00:00:10 +0900

    Splash animation

--- app/src/main/java/com/example/androidtest/SplashActivity.kt
+++ app/src/main/java/com/example/androidtest/SplashActivity.kt
@@ -5,24 +5,21 @@ import com.airbnb.lottie.LottieAnimationView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.androidtest.MainActivity
+import android.util.Log
+import com.example.androidtest.databinding.ActivitySplashBinding
 import com.example.androidtest.R
 import kotlin.time.Duration
 
 class SplashActivity : AppCompatActivity() {
 
     private val DURATION: Long = 1500
-
+    private lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_splash)
-
-       val loadingImage = findViewById<LottieAnimationView>(R.id.animationView)
-
-        // 애니메이션 시작
-        loadingImage.playAnimation()
-
-
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+        binding.animationView.speed=2.0F
+        binding.animationView.playAnimation()
         Handler().postDelayed({
             val intent = Intent(this,MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
