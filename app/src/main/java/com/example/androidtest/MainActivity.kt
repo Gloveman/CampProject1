d833b5c71c689c1ab1b1315af81c62e02b9031ed
gloveman <gloveman@postech.ac.kr>
2023-07-05 13:26:10 +0900

    add tab visibility change

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -10,13 +10,19 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock.sleep
+import android.util.Log
 import com.google.android.material.tabs.TabLayoutMediator
 import android.view.View
 import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.androidtest.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -37,20 +43,44 @@ class MainActivity : AppCompatActivity() {
                             0 -> {
                                 tab.text = "Contacts"
                                 tab.setIcon(R.drawable.contacticon)
-
+                                tab.id=0
+                                tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_LABELED
                             }
                             1 -> {
                                 tab.text = "Gallery"
-                                tab.setIcon(R.drawable.galleryicon)
+                                tab.id=1
+                                //
+                                tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
                             }
                             2 -> {
                                 tab.text = "Calendar"
-                                tab.setIcon(R.drawable.calendericon)
+                                tab.id=2
+                                //
+                                tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
                             }
                         }
 
                     }.attach()
+                    tl.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
+                        override fun onTabSelected(tab: Tab?) {
+                            tab?.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_LABELED
+                            when(tab?.id){
+                                0->tab.setIcon(R.drawable.contacticon)
+                                1->tab.setIcon(R.drawable.galleryicon)
+                                2->tab.setIcon(R.drawable.calendericon)
+                            }
+                        }
 
+                        override fun onTabUnselected(tab: Tab?) {
+                            tab?.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
+                            tab?.setIcon(R.drawable.nonvisibleicon)
+                        }
+
+                        override fun onTabReselected(tab: Tab?) {
+
+                        }
+
+                    })
 
                 } else {
                     requestPermissions(permArray, 1357)
@@ -70,25 +100,28 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(grantResults.all{it==0}){
+        if(grantResults.all{it==PackageManager.PERMISSION_GRANTED}){
             var vp = binding.viewPager
             var tl = binding.tabLayout
             vp.adapter = viewpageradapter(supportFragmentManager, lifecycle)
+
             window.statusBarColor= 0xff1A2028.toInt()
             TabLayoutMediator(tl, vp) { tab, position ->
                 when (position) {
                     0 -> {
                         tab.text = "Contacts"
                         tab.setIcon(R.drawable.contacticon)
-
+                        tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
                     }
                     1 -> {
                         tab.text = "Gallery"
                         tab.setIcon(R.drawable.galleryicon)
+                        tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
                     }
                     2 -> {
                         tab.text = "Calendar"
                         tab.setIcon(R.drawable.calendericon)
+                        tab.tabLabelVisibility=TabLayout.TAB_LABEL_VISIBILITY_UNLABELED
                     }
                 }
             }.attach()
